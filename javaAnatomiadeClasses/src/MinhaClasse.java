f1dc20e6fe7a5070ff62f85d1c616e3cdeaccbd2
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-01-12 08:29:55 -0300

    Atribuindo minha primeira classe em Java

---
+++ javaAnatomiadeClasses/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+    public static void main(String [] args){
+        System.out.println("Primeiras Linhas de codgio");
+    }
+    /*Primeiras linhas de código é meu "primeiro programa de Java", vou fazendo alterações e commits nesse projeto para que
+     tudo fique registrado*/
+
+}

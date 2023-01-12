bbf158485df46c26d6376620e42d2372cbb9a9d9
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-01-12 09:58:43 -0300

    Ultilização de variaveis básicas.

--- javaAnatomiadeClasses/src/MinhaClasse.java
+++ javaAnatomiadeClasses/src/MinhaClasse.java
@@ -1,9 +1,16 @@
 public class MinhaClasse {
 
     public static void main(String [] args){
-        System.out.println("Primeiras Linhas de codgio");
+
+        String NOME = "Jeremias";
+        int idade = 23;
+        int peso = 60;
+        System.out.println("O nome do nosso candidato é "+ NOME + ", a idade dele é ataualmente " + idade + " anos, e seu peso é de aproximadamente" +peso);
+        int anosPassados = 30;
+        idade = idade + anosPassados;
+        peso = peso + 15;
+        System.out.println("Porem depois de " + anosPassados + " anos, "+ NOME +" terá "+ idade +" anos de idade e "+ peso +" metros de peso");
     }
-    /*Primeiras linhas de código é meu "primeiro programa de Java", vou fazendo alterações e commits nesse projeto para que
-     tudo fique registrado*/
+    /*Utilização de variáveis para exibir mensagens na tela.*/
 
 }

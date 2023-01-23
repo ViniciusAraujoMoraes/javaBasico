8bd2cfc3c06bf9464765e71364769c6f20edf512
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-01-23 18:55:56 -0300

    Criando um projeto p/ exemplos de tipos/Variaveis

---
+++ javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
@@ -0,0 +1,11 @@
+package edu.vinicius.tipoVariaveis;
+//pasta que vai conter novos projetos relacionados a trilha de java basico.
+
+public class tipoVariaveis {
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+} 

fdaf0d586523c40b098565c8852a965342a83b6a
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-01-23 19:18:52 -0300

    commit para mostrar erro

--- javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
+++ javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
@@ -5,7 +5,11 @@ public class TiposVariaveis {
 
     public static void main(String[] args) {
         
-        
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
 
     }
 } 

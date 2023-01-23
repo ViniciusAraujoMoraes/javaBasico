68a1f4a073e5a4f2cf80436c101c931eed180cfb
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-01-23 19:01:45 -0300

    .

--- javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
+++ javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
@@ -1,7 +1,7 @@
 package edu.vinicius.tipoVariaveis;
 //pasta que vai conter novos projetos relacionados a trilha de java basico.
 
-public class tipoVariaveis {
+public class TiposVariaveis {
 
     public static void main(String[] args) {
         

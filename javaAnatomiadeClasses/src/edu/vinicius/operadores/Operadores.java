e52efd67b07e35d3a1733532eb7483be113e1d1b
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-02-06 17:39:10 -0300

    operadores 1

---
+++ javaAnatomiadeClasses/src/edu/vinicius/operadores/Operadores.java
@@ -0,0 +1,15 @@
+package edu.vinicius.operadores;
+
+public class Operadores {
+
+    public static void main(String[] args){
+
+        int numero = 5;
+
+        System.out.println(-numero);
+        System.out.println(numero);
+
+    }
+    //Demonstração que a negação da variável não afeta o número salvo na memoria e sim somente sua saída.
+    
+}

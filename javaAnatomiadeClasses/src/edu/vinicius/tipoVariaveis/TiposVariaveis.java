1834773b6634e49fb559f959c92511a97d255573
vinicius moraes <moraes.vinicius.ma@gmail.com>
2023-02-06 17:29:33 -0300

    nota de resolução de paramentos

--- javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
+++ javaAnatomiadeClasses/src/edu/vinicius/tipoVariaveis/TiposVariaveis.java
@@ -5,7 +5,7 @@ public class TiposVariaveis {
 
     public static void main(String[] args) {
         
-        // TiposEVariaveis.java
+        //Esse cogido tem como função mostrar que um short pode ser um int, mas um int não pode ser um short
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
